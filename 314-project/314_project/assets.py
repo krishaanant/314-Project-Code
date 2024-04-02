96e0ea5e70796a0625b443323896fce5f8223345
Krisha Anant <106985883+krishaanant@users.noreply.github.com>
2024-04-02 23:42:27 +0000

    Co-authored-by: Jimin Lee <jumonlala@users.noreply.github.com> Co-authored-by: sbhayani15 <sbhayani15@users.noreply.github.com> Co-authored-by: chloelsmith <chloelsmith@users.noreply.github.com> Co-authored-by: Tim Mellman <tim-mellman@users.noreply.github.com>

--- 314-project/314_project/assets.py
+++ 314-project/314_project/assets.py
@@ -1,8 +1,19 @@
 from dagster import asset
 import pandas as pd
 
+#test: chloe 
 @asset
 def load_data():
     data = pd.read_csv("/workspaces/314-Project-Code/314-project/data/healthcare-dataset-stroke-data.csv")
+    return data
 
-load_data()
+@asset
+# remove N/A rows Chloe 
+
+# gender and ever_married columns to binary form (0,1) and remove "other" for gender column Krisha Tim-(pytest)
+
+# remove id and smoking status columns Sonia
+
+# filter for adults (> 18) in age column jimin
+
+# ask about to_csv? 
