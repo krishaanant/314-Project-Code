23e73b49e90019f5994dc9efce5c62d7257cea3d
sbhayani15 <112509816+sbhayani15@users.noreply.github.com>
2024-04-03 18:31:00 +0000

    test_remove_cols -sonia

--- 314-project/314_project_tests/test_assets.py
+++ 314-project/314_project_tests/test_assets.py
@@ -4,6 +4,6 @@ import pandas as pd
 def test_remove_cols():
     data = pd.DataFrame({"id" : [10], "smoking": ["yes"], "age" : [18], "gender" : [1]})
     list = ["age", "smoking"]
-    pd.testing.assert_frame_equal(remove_cols(list, data), pd.DataFrame({"id": [10], "gender": [1]}))
+    pd.testing.assert_frame_equal(remove_cols(data, list), pd.DataFrame({"id": [10], "gender": [1]}))
 
 
